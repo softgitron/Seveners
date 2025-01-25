80580384345360d929cd726202f9c6afb9f3ab9a
Juuso Ylikoski <juuso.ylikoski@twoday.com>
2025-01-25 17:39:24 +0200

    Iha sama

--- Juuso/TorpedoLauncher.cs
+++ Juuso/TorpedoLauncher.cs
@@ -1,11 +1,23 @@
 using Godot;
 using System;
 
-public partial class SubmarineShooting : CharacterBody2D
+public partial class TorpedoLauncher : Node2D
 {
-	//string x = ResourceLoader.Load("res://Juuso/Torpedo.tscn", );
-	public override void _PhysicsProcess(double delta)
+	PackedScene bulletScene = (PackedScene)GD.Load("res://Juuso/Torpedo.tscn");
+	public override void _Process(double delta)
 	{
-		base._PhysicsProcess(delta);
+		if (Input.IsActionJustPressed("shoot_0")){
+			GD.Print("Shoow");
+			Fire();
+		}
+	}
+
+	public void Fire(){
+		Torpedo torpedo = (Torpedo)bulletScene.Instantiate();
+		torpedo.pos = GlobalPosition;
+		torpedo.direction = Rotation;
+        GD.Print(Rotation);
+		torpedo.rotation = GlobalRotation;
+		GetTree().Root.AddChild(torpedo);
 	}
 }
