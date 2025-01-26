082008386e842102929bdcd86438c4a03990e04b
Lauri Tahvanainen <48516187+LauriTahvanainen@users.noreply.github.com>
2025-01-26 13:20:19 +0200

    Sonar to follow player

--- Lauri/Radar.cs
+++ Lauri/Radar.cs
@@ -1,20 +1,44 @@
 using Godot;
 using Godot.Collections;
 using System;
+using System.Diagnostics;
 
 public partial class Radar : Sprite2D
 {
 	private Dictionary<ulong, RadarBlip> _NodeIdToBlip;
 	PackedScene blipScene = (PackedScene)GD.Load("res://Lauri/RadarBlip.tscn");
+	private HumanControllableSubmarine _playerNode = null;
+	private Camera2D _radarCameraNode = null;
+	[Export]
+	private NodePath _playerNodePath = new NodePath("");
+	[Export]
+	private NodePath _radarCameraPath = null;
 
 	public override void _Ready()
 	{
 		_NodeIdToBlip = new Dictionary<ulong, RadarBlip>();
+		_playerNode = GetTree().Root.GetNode<HumanControllableSubmarine>("Main2/Player");
+		Debug.Assert(_playerNode != null, "Player node can not be null! Radar can not work!");
+		_radarCameraNode = GetNodeOrNull<Camera2D>(_radarCameraPath);
+		Debug.Assert(_radarCameraNode != null, "Radar camera node can not be null! Radar can not work!");
 	}
 
 	public override void _Process(double delta)
 	{
+		SnapCameraToPlayer();
 		HandleRadarEntityLocations();
+	}
+	
+	private void SnapCameraToPlayer()
+	{
+		//Debug.Print("Camera global position: " + _radarCameraNode.GlobalPosition.ToString());
+		//Debug.Print("Player global position: " + _playerNode.GlobalPosition.ToString());
+		//_radarCameraNode.Position = _playerNode.GlobalPosition;
+		//this.GlobalPosition = _playerNode.GlobalPosition;
+		//_radarCameraNode.Offset = _playerNode.Position;
+		Vector2 radarOffset = _playerNode.GlobalPosition;
+		Vector2 radarOffsetScaledToRadar = radarOffset / 10;
+		_radarCameraNode.Offset = radarOffsetScaledToRadar;
 	}
 
 	private void HandleRadarEntityLocations()
