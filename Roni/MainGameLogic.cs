dde46e63cdf5ccfad6ad3c9cb24e28d39a8394f7
johan1954 <tomi.enberg@gmail.com>
2025-01-26 12:55:02 +0200

    Add remove child

--- Roni/MainGameLogic.cs
+++ Roni/MainGameLogic.cs
@@ -107,6 +107,11 @@ public partial class MainGameLogic : Node
 			level++;
 			EmitSignal(SignalName.LevelCanged, level);
 			Initialize();
+			var children = SpawnerService.GetChildren();
+			foreach (var child in children)
+			{
+				child.QueueFree();
+			}
 		}
 	}
 
